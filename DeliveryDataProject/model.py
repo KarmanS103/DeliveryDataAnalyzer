dc507764be4b034b8cb3c24550a3ac8375b121e9
Karman Singh <karmansingh@icloud.com>
2020-10-19 23:40:21 -0400

    Changing structure to individual files for each report

--- DeliveryDataProject/model.py
+++ DeliveryDataProject/model.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+import glob
 
 
 # Get file path for folder instead and read in all of the files from the folder
-class DoorDashImporter:
-    def begin_import(self):
-        doordash_df = pd.read_csv("Payment Details/testdata.csv")
-        print(doordash_df.head(5))
+def ubereats_importer():
+    payment_details_df = payment_details_importer()
+
+
+def payment_details_importer():
+    list_of_df = []
+
+    for name in glob.glob("Payment Details/*.csv"):
+        list_of_df.append(pd.read_csv(name))
+
+    payment_details_df = pd.concat(list_of_df)
 
 
 if __name__ == "__main__":
-    d1 = DoorDashImporter()
-    d1.begin_import()
+    ubereats_importer()
